078929008b2bd724a224b6ed0f5faee4e327174b
Sagal Afrah <afrahsagal@gmail.com>
2021-12-12 01:03:29 -0500

    added basic component to build on for daylistitem & imported it to index.js

--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import DayListItem from "components/DayListItem";
 
 import "index.scss";
 
