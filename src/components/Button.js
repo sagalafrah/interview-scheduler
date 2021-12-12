2d8532f2ec8635cb349b52328a83851a09273a36
Sagal Afrah <afrahsagal@gmail.com>
2021-12-12 00:55:18 -0500

    changed disabled click to not fire a click

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,14 +4,22 @@ import "components/Button.scss";
 
 export default function Button(props) {
    let buttonClass = "button";
- 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
- 
-   if (props.danger) {
-     buttonClass += " button--danger";
-   }
- 
-   return <button className={buttonClass}>{props.children}</button>;
- }
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+}
