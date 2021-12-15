d93fdabbe0cf5e63fcebef7d05c609aeee4c1c06
Sagal Afrah <afrahsagal@gmail.com>
2021-12-15 16:29:24 -0500

    added useState and debugging

--- src/components/appointment/form.js
+++ src/components/appointment/form.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from 'react';
 
 import Button from "../Button";
 import InterviewerList from "../InterviewerList";
 
 export default function Form(props) {
+    const [student, setStudent] = useState(props.student || "");
+    const [interviewer, setInterviewer] = useState(props.interviewer || null);
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
@@ -13,12 +15,14 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
+            value={Name}
+            onChange={event => setName(event.target.value)}
           />
     </form>
     <InterviewerList 
           interviewers={props.interviewers} 
-          value={props.interviewer} 
+          value={interviewer} 
+          onChange={setInterviewer} 
         />
   </section>
   <section className="appointment__card-right">
