62e4291d5f217393613178a305d8b823afcb2ab8
Sagal Afrah <afrahsagal@gmail.com>
2021-12-15 16:46:33 -0500

    imported header, show and empty from index and added fragment

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { Fragment } from 'react'
 import "./styles.scss";
+
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 
 
