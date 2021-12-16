4bcd8a3924493eefc58c9cba18df5b49f9a74057
Sagal Afrah <afrahsagal@gmail.com>
2021-12-15 19:23:41 -0500

    added helper function and unit test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export const getAppointmentsForDay = (state, day) => {
+    const dayObj = state.days.find(elem => elem.name === day);
+  
+    if (!dayObj) {
+      return [];
+    }
+  const appointmentID = dayObj.appointments;
+  const appointmentsForEachDay = [];
+  
+    for (const id in state.appointments) {
+      if (appointmentID.includes(Number(id))) {
+        appointmentsForEachDay.push(state.appointments[id]);
+      }
+    }
+  
+    return appointmentsForEachDay;
+  };
