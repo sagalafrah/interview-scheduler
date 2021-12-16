ffa4ec582ca4d29ed1b39d30d291c641e1ed9e04
Sagal Afrah <afrahsagal@gmail.com>
2021-12-15 19:40:26 -0500

    fixed major bug

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,24 +1,69 @@
-import React, { useState, useEffect} from "react";
-import axios from "axios";
-import DayList from "components/DayList";
-import Appointment from "components/appointment"
+import React, { useState } from "react";
 import "components/Application.scss";
-import "components/DayList";
+import DayList from "components/DayList";
+import Appointment from "components/appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
-  }) 
-  useEffect(() => {
-    axios.get("/api/days")
-    .then(response => state.setDays(response.data));
-  }, [])
-  return ( 
+  return (
     <main className="layout">
       <section className="sidebar">
       <img
@@ -29,9 +74,9 @@ export default function Application(props) {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
 <DayList
-    days={state.days}
-    day={state.day}
-    setDay={state.setDay}
+  days={days}
+  day={"Monday"}
+  setDay={day => console.log(day)}
 />
 </nav>
 <img
@@ -41,7 +86,13 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      {appointments.map(appointment => (
+          <Appointment
+            key={appointment.id}
+            {...appointment}
+          />
+        ))}
+        <Appointment id="last" time="5pm" />
       </section>
     </main>
   );
