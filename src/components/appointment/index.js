92b4aec09faca05378c39742cc766c930ce5838d
Sagal Afrah <afrahsagal@gmail.com>
2021-12-15 21:46:30 -0500

    added transitions

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -3,6 +3,7 @@ import useVisualMode from "hooks/useVisualMode";
 import Header from "./header";
 import Show from "./show";
 import Empty from "./empty";
+import Form from "./form"
 
 
 
@@ -11,6 +12,7 @@ import "./styles.scss";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 
 
@@ -22,12 +24,21 @@ export default function Appointment(props) {
     return (
       <article className="appointment">
         <Header time={props.time} />
-        {mode === EMPTY && <Empty/>}
+        {mode === EMPTY && (
+        <Empty
+          onAdd={() => transition(CREATE)}
+        />
+      )}
         {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           /> 
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+        />
       )}
       </article>
     )
