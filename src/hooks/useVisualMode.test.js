f71eb081e3ed70a4e3b2203e6482b19e4200ffc4
Sagal Afrah <afrahsagal@gmail.com>
2021-12-15 21:27:01 -0500

    add failing test, fix later

--- src/hooks/useVisualMode.test.js
+++ src/hooks/useVisualMode.test.js
@@ -2,6 +2,7 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import useVisualMode from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
+const SECOND = "SECOND";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -9,3 +10,9 @@ test("useVisualMode should initialize with default value", () => {
   expect(result.current.mode).toBe(FIRST);
 });
 
+test("useVisualMode should transition to another mode", () => {
+    const { result } = renderHook(() => useVisualMode(FIRST));
+  
+    act(() => result.current.transition(SECOND));
+    expect(result.current.mode).toBe(SECOND);
+  });
