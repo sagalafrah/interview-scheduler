80dfda365656b6e31c4ee880c8503a0853e3e33f
Sagal Afrah <afrahsagal@gmail.com>
2021-12-15 21:35:51 -0500

    updated json files & added tests and usevisualmode functions and transitions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,36 @@
 import { useState } from "react";
+
 export default function useVisualMode(initial) {
     const [mode, setMode] = useState(initial);
+    const [history, setHistory] = useState([initial]);
+    const transition = (newMode, replace = false) => {
+      setMode(newMode);
   
-    return { mode };
+      setHistory(history => {
+        if (replace) {
+          const newHistory = [...history];
+          newHistory.splice(-1, 1, newMode);
+          return newHistory;
+        } else {
+          return [...history, newMode];
+        }
+      });
+    };
+  
+    const back = () => {
+      setHistory(history => {
+        const newHistory =
+          history.length > 1 ? [...history].slice(0, -1) : [...history];
+  
+        setMode(newHistory[newHistory.length - 1]);
+  
+        return newHistory;
+      });
+    };
+  
+    return {
+      mode,
+      transition,
+      back
+    };
   }
